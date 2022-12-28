11d28cbc1a93c1efeed083b0693705d72ee0b74f
07Chriiiiis <christianjohnny_and!@hotmail.com>
2022-12-28 02:18:11 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'screens/categories_meals_screen.dart';
+import 'screens/categories_screen.dart';
+import 'utils/app_routes.dart';
+import 'screens/meal_detail_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Vamos Cozinhar?',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(secondary: Colors.amber),
+        fontFamily: 'Kanit',
+        canvasColor: Color.fromRGBO(255, 255, 230, 2),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              subtitle1: TextStyle(
+                fontSize: 25,
+                fontFamily: 'YanoneKaffeesatz',
+              ),
+            ),
+      ),
+      //home: CategoriesScreen(),
+      routes: {
+        AppRoutes.HOME: (context) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEALS_DETAIL: (ctx) => MealDetailScreen(),
+      },
+
+      //metodos usados para informar como agir quando não encontrar a rota por qualquer motivo
+      /* onGenerateRoute: (settings) {
+        if (settings.name == '/alguma-coisa') {
+          return null;
+        } else {
+          return MaterialPageRoute(
+            builder: (_) {
+              return CategoriesScreen();
+            },
+          );
+        }
+      }, */
+      /* onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (_) {
+            return CategoriesScreen();
+          },
+        );
+      }, */
+    );
+  }
+}
